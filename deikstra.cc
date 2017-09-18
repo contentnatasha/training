5b76949a84da44b4f167bb6e7a99f82cbc2df7b7
Natasha <natasha.serebryanaya@gmail.com>
2017-09-18 19:37:47 +0000

    add deikstra.cc

---
+++ deikstra.cc
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+class Matrix {
+public:
+	Matrix(size_t n) : N(n), v(n*n) {
+	}
+	int& operator() (size_t row, size_t col) {
+		return v[row*N + col];
+	}
+	const int& operator() (size_t row, size_t col) const {
+		return v[row*N + col];
+	}
+	size_t size() const {
+		return N;
+	}
+private:
+	size_t N;
+	std::vector<int> v;
+};
+
+void ReadMatrix(Matrix &m) {
+	for (size_t row = 0; row < m.size(); row++) {
+		for (size_t col = 0; col < m.size(); col++) {
+			std::cin >> m(row,col);
+		}
+	}
+}
+
+void PrintMatrix(const Matrix &m) {
+	for (size_t row = 0; row < m.size(); row++) {
+		for (size_t col = 0; col < m.size(); col++) {
+			std::cout << m(row,col) << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+int Deikstra(Matrix m, int start, int finish) {
+	size_t N = m.size();
+	std::vector<bool> visited(N, false); 
+	std::vector<int> weights(N, INT_MAX);
+	weights[start] = 0;
+	for (size_t i = 0; i < N; i++) { // we don't use i anywhere here, just to make sure we visited all N vertices
+		//finding the vertice with minimal weight
+		int cur_weight = INT_MAX;
+		int cur_vertice;
+		for (size_t j = 0; j < N; j++) {
+			if (!visited[j] && (weights[j] < cur_weight) ) {
+				cur_weight = weights[j];
+				cur_vertice = j;
+			}
+		}
+		visited[cur_vertice] = true;
+		// update all the vertice neighbores of cur_vertice
+		for (size_t j = 0; j < N; j++) {
+			if (!visited[j] && ( m(cur_vertice, j)!=-1 ) && ( weights[cur_vertice] + m(cur_vertice, j) < weights[j]) ) {
+				weights[j] = weights[cur_vertice] + m(cur_vertice, j);
+			}
+		}
+	}
+	return (weights[finish] == INT_MAX) ? -1 : weights[finish];
+}
+
+int main() {
+	size_t NVertice;
+	std::cin >> NVertice;
+	int start, finish;
+	std::cin >> start;
+	std::cin >> finish;
+	Matrix m(NVertice);
+	ReadMatrix(m);
+	//std::cout << m(0,2) << " " << m(1,0) << std::endl;
+	//PrintMatrix(m);
+	std::cout << Deikstra(m, start-1, finish-1) << std::endl;
+}
